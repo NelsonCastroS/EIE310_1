571132bf0148f296be35ee2cac80aa428e1a6a42
NelsonCastroS <37382215+NelsonCastroS@users.noreply.github.com>
2018-03-14 17:19:04 -0300

    Add files via upload

---
+++ ejercicio2.m
@@ -0,0 +1,12 @@
+%mi rut es:19.330.298-k
+vi=1; 
+a=0.9; 
+dt=10e-3;
+t(1)=0;
+vf(1)=0;
+for i=2:41
+    t(i)=(i-1)*dt;
+    vf(i)=1-a*vi+a*vf(i-1); 
+end
+close; stem (t,vf);
+xlabel("seg"), ylabel("Volts"); grid; 
